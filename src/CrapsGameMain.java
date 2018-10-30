818b7c029bbf5c2b2cc55c97977d36628e45a3f1
pawasthi20 <43074073+pawasthi20@users.noreply.github.com>
2018-10-30 02:13:19 -0400

    Add files via upload

--- src/CrapsGameMain.java
+++ src/CrapsGameMain.java
@@ -9,57 +9,70 @@ public class CrapsGameMain
 	public static void main(String[] args) 
 	{
 		Scanner in = new Scanner(System.in);
+		boolean replay = true;
 		
+		//Dice Objects
 		Dice dice1 = new Dice();
 		Dice dice2 = new Dice();
-		int roll1 = dice1.roll();
-		int roll2 = dice2.roll();
-		int sum = roll1 + roll2;
-		System.out.print("Press enter to roll.");
-		in.nextLine();
-		System.out.println(sum);
-		
-		//First Roll
-		if (sum == 2 || sum == 12)
+		int roll1;
+		int roll2;
+		int sum;
+
+		//Replay Loop
+		while (replay)
 		{
-			System.out.println("You loose. If you want to play again, press enter.");
+			System.out.print("Welcome to CrapsGame. Press enter to roll.");
 			in.nextLine();
-		}
-		if (sum == 7 || sum == 11)
-		{
-			System.out.print("You win. If you want to play again, press enter.");
-			in.nextLine();
-		}
-		else 
-		{
-			System.out.println("You get a point.");
-			System.out.print("Press enter to roll.");
-			in.nextLine();
-			System.out.println(sum);
-			int point = sum;
 			roll1 = dice1.roll();
-			roll2 = dice2.roll();
+			roll2 = dice2.roll(); 
 			sum = roll1 + roll2;
-			if(sum == 7)
+			System.out.println("You rolled: " + sum);
+			
+			//First Roll
+			if (sum == 2 || sum == 12)
 			{
-				System.out.println("You loose. If you want to play again, press enter.");
-			}
-			if(sum == point)
-			{
-				System.out.println("You win. If you want to play again, press enter.");
-			}
-			while(sum != 7 && sum != point) 
-			{
-				//Roll Again
-				System.out.print("Press enter to roll.");
+				System.out.println("You loose.");
 				in.nextLine();
-				roll1 = dice1.roll();
-				roll2 = dice2.roll();
-				sum = roll1 + roll2;
-				System.out.println(sum);
 			}
+			if (sum == 7 || sum == 11)
+			{
+				System.out.print("You win.");
+				in.nextLine();
+			}
+			else 
+			{
+				System.out.println("Your point is: " + sum);
+				int point = sum;
+				while(sum != 7) 
+				{
+					//Subsequent Roll
+					System.out.print("Press enter to roll again.");
+					in.nextLine();
+					roll1 = dice1.roll();
+					roll2 = dice2.roll();
+					sum = roll1 + roll2;
+					System.out.println("You rolled " + sum);
+				}
+				if(sum == 7)
+				{
+					System.out.println("You loose.");
+				}
+				if(sum == point)
+				{
+					System.out.println("You win.");
+				}
+			}
+			//Prompting Replay
+			System.out.println("Do you want to replay? Say yes or no.");
+			String i = in.nextLine();
+		    if(i.equals("yes")) 
+		    {
+		        replay = true;
+		    }
+		    else if(i.equals("no")) 
+			{
+			    replay = false;
+			}			
 		}
 	}
 }
-
-//How do I get the game to restart?
